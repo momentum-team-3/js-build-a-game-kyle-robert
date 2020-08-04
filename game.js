b6746b19bd0bc524b155c22c3645ff097bf399d3
kldavis52 <kyle.linnard.davis@gmail.com>
2020-08-04 14:41:03 -0400

    game updates, canvas not working yet

--- game.js
+++ game.js
@@ -1,15 +1,83 @@
+// Global constants and variables
+// The document's canvas element
+const canv = document.querySelector('#canvas')
+// small helper for getting a new context
+const getctx = () => canv.getContext('2d')
+document.body.appendChild(canv)
+const HEIGHT = canv.height
+const WIDTH = canv.width
+const CHARTREUSE = 'rgb(127,255,0)'
+var playerAlive = true
 
-
-
-function keyPresslistener(event) {
-  if (event.key === "ArrowRight") {
-    player.X += 10
-  } else (event.key === "ArrowLeft") {
-    player.X -= 10
+function createPlayer (x, y, height, width, color, isPlayer) {
+  return {
+    X: x,
+    Y: y,
+    Height: height,
+    Width: width,
+    xUpper: WIDTH - width,
+    yUpper: HEIGHT - height,
+    xLower: 0,
+    yLower: 0,
+    color: color,
+    isPlayer: isPlayer
   }
 }
 
+const player = createPlayer(250, 950, 100, 200, CHARTREUSE, true)
 
-window.addEventListener('keydown', keyPresslistener(KeyboardEvent))
+// function createPlayer (x, y, playerWidth, isPlayer) {
+//   const player = {
+//     X: x,
+//     Y: y,
+//     Width: playerWidth,
+//     xUpper: WIDTH - playerWidth,
+//     xLower: 0,
+//     isPlayer: isPlayer
+//   }
+// }
 
-window.addEventListener('keyup',)
+function keyPresslistener (event) {
+  if (event.key === 'ArrowRight') {
+    player.X += 10
+  } else if (event.key === 'ArrowLeft') {
+    player.X -= 10
+  }
+  if (player.X > player.xUpper) {
+    player.X = player.xLower
+  }
+  if (player.X < player.xLower) {
+    player.X = player.xUpper
+  }
+}
+
+// get a new 2d drawing context
+function getContext () {
+  return canv.getContext('2d')
+}
+
+function clearScreen () {
+  const context = getContext()
+  context.clearRect(0, 0, WIDTH, HEIGHT)
+}
+
+createPlayer(250, 950, 50, 50, true)
+
+function drawPlayer (x, y, width, height, color) {
+  const ctx = getContext()
+
+  ctx.fillStyle = color
+  ctx.fillRect(x, y, width, height)
+}
+
+function drawFrame () {
+  clearScreen()
+  // if (areColliding(player, badguy)) {
+  //   console.log('Oh no, an emergency!')
+  // }
+  drawPlayer(player.X, player.Y, player.Width, player.Height, player.color)
+}
+
+window.addEventListener('keydown', keyPresslistener)
+
+const animationID = window.setInterval(drawFrame, 50)
