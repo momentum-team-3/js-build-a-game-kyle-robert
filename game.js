6951d1425818a4ba87c0eca0691877602ed28815
kldavis52 <kyle.linnard.davis@gmail.com>
2020-08-05 18:07:23 -0400

    add some logic for a blaster

--- game.js
+++ game.js
@@ -7,13 +7,25 @@ document.body.appendChild(canv)
 const HEIGHT = canv.height
 const WIDTH = canv.width
 let animationID
+const blasterVelocity = [0, 3]
 const playerBlaster = new Image()
 const playerImg = new Image()
 playerBlaster.src = 'Images/Space_rage128px/Blaster_plasma_purple.png'
 playerImg.src = 'Images/Space_rage128px/PlayerRed_Frame_01_png_processed.png'
 var playerAlive = true
 const baseImageSize = 128 // 128 px
-const scale = 0.25 // scale image to 32 px
+
+function makeBlaster (x, y) {
+  const blaster = {
+    x: x,
+    y: y,
+    velocity: blasterVelocity,
+    numLives: 1,
+    blasterImg: playerBlaster,
+    isPlayer: false
+  }
+  return blaster
+}
 
 function makeShip (x, y, image, isPlayer) {
   const ship = {
@@ -21,7 +33,7 @@ function makeShip (x, y, image, isPlayer) {
     y: y,
     playerImg: image,
     isPlayer: isPlayer,
-    radius: () => Math.floor((baseImageSize * scale) / 2)
+    radius: () => Math.floor((baseImageSize) / 2)
   }
   return ship
 }
@@ -30,28 +42,40 @@ function createPlayer (x, y, image, isPlayer) {
   var playerShip = makeShip(
     x,
     y,
-    playerImg,
+    image,
     isPlayer)
 
   // playerShip.playerImg = playerImg
   playerShip.numLives = 3
-  playerShip.height = baseImageSize * scale
-  playerShip.width = baseImageSize * scale
+  playerShip.height = baseImageSize
+  playerShip.width = baseImageSize
   playerShip.xUpper = WIDTH - playerShip.width
   playerShip.yUpper = HEIGHT - playerShip.height
   playerShip.xLower = 0
   playerShip.yLower = 0
 
+  playerShip.blaster = makeBlaster(playerShip.x, playerShip.y)
   return playerShip
 }
 
 var player = createPlayer(250, 575, playerImg, true)
+
+function updateBlasterPosition () {
+  player.blaster.x += player.blaster.velocity[0]
+  player.blaster.y += player.blaster.velocity[1]
+}
+
+function fireBlaster () {
+  updateBlasterPosition()
+}
 
 function keyPresslistener (event) {
   if (event.key === 'ArrowRight') {
     player.x += 10
   } else if (event.key === 'ArrowLeft') {
     player.x -= 10
+  } else if (event.key === 'ArrowUp') {
+    fireBlaster()
   }
   if (player.x > player.xUpper) {
     player.x = player.xLower
@@ -81,8 +105,14 @@ function drawFrame () {
   // if (areColliding(player, badguy)) {
   //   console.log('Oh no, an emergency!')
   // }
+  updateBlasterPosition()
   drawPlayerImage(player.playerImg, player.x, player.y)
 }
+
+// if (!playerAlive) {
+//   alert('Game over!')
+//   window.clearInterval(animationID)
+// }
 
 window.addEventListener('keydown', keyPresslistener)
 
