a5843c523c3225cd15200bed65c13d4e04495d02
kldavis52 <kyle.linnard.davis@gmail.com>
2020-08-05 21:34:11 -0400

    blaster semi-working

--- game.js
+++ game.js
@@ -7,35 +7,29 @@ document.body.appendChild(canv)
 const HEIGHT = canv.height
 const WIDTH = canv.width
 let animationID
-const blasterVelocity = [0, 3]
-const playerBlaster = new Image()
+const blasterVelocity = [0, -2]
+const playerBlasterImg = new Image()
 const playerImg = new Image()
 const redEnemyImg = new Image()
 const greenEnemyImg = new Image()
 greenEnemyImg.src = 'Images/Space_rage128px/green_enemy.png'
 redEnemyImg.src = 'Images/Space_rage128px/red_enemy.png'
-playerBlaster.src = 'Images/Space_rage128px/Blaster_plasma_purple.png'
+playerBlasterImg.src = 'Images/Space_rage128px/Blaster_plasma_purple.png'
 playerImg.src = 'Images/Space_rage128px/PlayerRed_Frame_01_png_processed.png'
 var playerAlive = true
 const baseImageSize = 128 // 128 px
+var blaster
 
-class Blaster {
+class PlayerBlaster {
   constructor (x, y) {
     this.x = x
     this.y = y
+    this.velocity = blasterVelocity
+    this.numLives = 1
+    this.blasterImg = playerBlasterImg
+    this.isPlayer = false
+    return this
   }
-}
-
-function makeBlaster (x, y) {
-  const blaster = {
-    x: x,
-    y: y,
-    velocity: blasterVelocity,
-    numLives: 1,
-    blasterImg: playerBlaster,
-    isPlayer: false
-  }
-  return blaster
 }
 
 function makeShip (x, y, image, isPlayer) {
@@ -65,21 +59,10 @@ function createPlayer (x, y, image, isPlayer) {
   playerShip.xLower = 0
   playerShip.yLower = 0
 
-  playerShip.blaster = makeBlaster(playerShip.x, playerShip.y)
-
   return playerShip
 }
 
 var player = createPlayer(250, 575, playerImg, true)
-
-function updateBlasterPosition () {
-  player.blaster.x += player.blaster.velocity[0]
-  player.blaster.y += player.blaster.velocity[1]
-}
-
-function fireBlaster () {
-  updateBlasterPosition()
-}
 
 function keyPresslistener (event) {
   if (event.key === 'ArrowRight') {
@@ -87,7 +70,7 @@ function keyPresslistener (event) {
   } else if (event.key === 'ArrowLeft') {
     player.x -= 10
   } else if (event.key === ' ') {
-    fireBlaster()
+    blaster = new PlayerBlaster(player.x, player.y)
   }
   if (player.x > player.xUpper) {
     player.x = player.xLower
@@ -107,7 +90,7 @@ function clearScreen () {
   context.clearRect(0, 0, WIDTH, HEIGHT)
 }
 
-function drawPlayerImage (image, x, y) {
+function drawImage (image, x, y) {
   const ctx = getContext()
   ctx.drawImage(image, x, y)
 }
@@ -117,8 +100,12 @@ function drawFrame () {
   // if (areColliding(player, badguy)) {
   //   console.log('Oh no, an emergency!')
   // }
-  updateBlasterPosition()
-  drawPlayerImage(player.playerImg, player.x, player.y)
+  drawImage(player.playerImg, player.x, player.y)
+  drawImage(blaster.blasterImg, blaster.x, blaster.y)
+  if (blaster) {
+    blaster.x += blaster.velocity[0]
+    blaster.y += blaster.velocity[1]
+  }
 }
 
 // if (!playerAlive) {
